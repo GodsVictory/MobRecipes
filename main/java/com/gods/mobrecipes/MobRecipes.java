8cdab4610701a87f12d0b2c3c3392fa348b2c4ee
GodsVictory <thegodsvictory@gmail.com>
2016-11-23 13:00:22 -0600

    remove unneeded lines

--- main/java/com/gods/mobrecipes/MobRecipes.java
+++ main/java/com/gods/mobrecipes/MobRecipes.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class MobRecipes {
 	public static final String MODID = "mobrecipes";
 	public static final String NAME = "mobrecipes";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.3";
 
 	@Mod.Instance(MODID)
 	public static MobRecipes instance;

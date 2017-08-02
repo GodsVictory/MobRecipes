8a4b3923ccdaf9edb23c08b7227b03ca26d86393
GodsVictory <thegodsvictory@gmail.com>
2017-08-02 11:18:31 -0500

    update to 1.12

--- src/main/java/com/gods/mobrecipes/MobRecipes.java
+++ src/main/java/com/gods/mobrecipes/MobRecipes.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 public class MobRecipes {
   public static final String MODID = "mobrecipes";
   public static final String NAME = "mobrecipes";
-  public static final String VERSION = "0.0.3";
+  public static final String VERSION = "1.0.0";
 
   @Mod.Instance(MODID)
   public static MobRecipes instance;
